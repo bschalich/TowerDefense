dbc27bab5f2d5549bcc18a5b92610758aad3f4d7
Garrett Summers <gsummers@calpoly.edu>
2013-12-06 21:05:19 -0800

    Added animations for every enemy we have so far, they have random speeds and health. Also added waves just to test out all the animations.

--- js/enemy.js
+++ js/enemy.js
@@ -68,19 +68,189 @@ var Enemy = Class.create(Sprite, {
       // Use 0.0009 instead of 0.001
       var movement = (event.elapsed * 0.0009) * this.speed;
       switch (this.direction) {
-         case UP   : this.y -= movement; break;
-         case DOWN : this.y += movement; break;
-         case LEFT : this.x -= movement; break;
-         case RIGHT: this.x += movement; break;
+         case UP   : this.y -= movement;
+                     this.frame = this.anim[this.direction*20 + (this.age%20)]; break;
+         case DOWN : this.y += movement; 
+                     this.frame = this.anim[this.direction*20 + (this.age%20)]; break;
+         case LEFT : this.x -= movement; 
+                     this.frame = this.anim[this.direction*20 + (this.age%20)]; break;
+         case RIGHT: this.x += movement;
+                     this.frame = this.anim[this.direction*20 + (this.age%20)]; break;
       }
       // this.x = Math.ceil(this.x);
       // this.y = Math.ceil(this.y);
 	}
 });
 
+//Stone Age
+
+var KabutoEnemy = Class.create(Enemy, {
+	initialize: function(map, pathTile) {
+		Enemy.apply(this, ['assets/enemies/kabutoSheet.png', 25, 30, map, pathTile]);
+
+      this.anim = [
+         9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 10, 10, 10, 10, 10, //right
+         6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 7, 7, 7, 7, 7,//left
+         3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 4, 4, 4, 4, 4, //up
+         0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1];//down
+      
+		this.addEventListener(Event.ENTER_FRAME, this.walk);
+	}
+});
+
+var OmanyteEnemy = Class.create(Enemy, {
+	initialize: function(map, pathTile) {
+		Enemy.apply(this, ['assets/enemies/omanyteSheet.png', 30, 30, map, pathTile]);
+
+      this.anim = [
+         9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 10, 10, 10, 10, 10, //right
+         6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 7, 7, 7, 7, 7,//left
+         3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 4, 4, 4, 4, 4, //up
+         0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1];//down
+      
+		this.addEventListener(Event.ENTER_FRAME, this.walk);
+	}
+});
+
+var BastidonEnemy = Class.create(Enemy, {
+	initialize: function(map, pathTile) {
+		Enemy.apply(this, ['assets/enemies/bastidonSheet.png', 30, 30, map, pathTile]);
+
+      this.anim = [
+         6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, //right
+         4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, //left
+         2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, //up
+         0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1];//down
+      
+		this.addEventListener(Event.ENTER_FRAME, this.walk);
+	}
+});
+
+var AerodactylEnemy = Class.create(Enemy, {
+	initialize: function(map, pathTile) {
+		Enemy.apply(this, ['assets/enemies/aerodactylSheet.png', 30, 30, map, pathTile]);
+
+      this.anim = [
+         9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 10, 10, 10, 10, 10, //right
+         6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 7, 7, 7, 7, 7,//left
+         3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 4, 4, 4, 4, 4, //up
+         0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1];//down
+      
+		this.addEventListener(Event.ENTER_FRAME, this.walk);
+	}
+});
+
+//Industrial Age
+
+var AronEnemy = Class.create(Enemy, {
+	initialize: function(map, pathTile) {
+		Enemy.apply(this, ['assets/enemies/aronSheet.png', 50, 30, map, pathTile]);
+
+      this.anim = [
+         6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, //right
+         4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, //left
+         2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, //up
+         0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1];//down
+      
+		this.addEventListener(Event.ENTER_FRAME, this.walk);
+	}
+});
+
 var AggronEnemy = Class.create(Enemy, {
 	initialize: function(map, pathTile) {
 		Enemy.apply(this, ['assets/enemies/aggronSheet.png', 30, 40, map, pathTile]);
+
+      this.anim = [
+         9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 10, 10, 10, 10, 10, //right
+         6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 7, 7, 7, 7, 7,//left
+         3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 4, 4, 4, 4, 4, //up
+         0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1];//down
+      
+		this.addEventListener(Event.ENTER_FRAME, this.walk);
+	}
+});
+
+var SkarmoryEnemy = Class.create(Enemy, {
+	initialize: function(map, pathTile) {
+		Enemy.apply(this, ['assets/enemies/skarmorySheet.png', 30, 40, map, pathTile]);
+
+      this.anim = [
+         9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 10, 10, 10, 10, 10, //right
+         6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 7, 7, 7, 7, 7,//left
+         3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 4, 4, 4, 4, 4, //up
+         0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1];//down
+      
+		this.addEventListener(Event.ENTER_FRAME, this.walk);
+	}
+});
+
+var RyhornEnemy = Class.create(Enemy, {
+	initialize: function(map, pathTile) {
+		Enemy.apply(this, ['assets/enemies/ryhornSheet.png', 50, 30, map, pathTile]);
+
+      this.anim = [
+         6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, //right
+         4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, //left
+         2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, //up
+         0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1];//down
+      
+		this.addEventListener(Event.ENTER_FRAME, this.walk);
+	}
+});
+
+//Neon Age
+
+var GroudonEnemy = Class.create(Enemy, {
+	initialize: function(map, pathTile) {
+		Enemy.apply(this, ['assets/enemies/groudonSheet.png', 50, 30, map, pathTile]);
+
+      this.anim = [
+         9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 10, 10, 10, 10, 10, //right
+         6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 7, 7, 7, 7, 7,//left
+         3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 4, 4, 4, 4, 4, //up
+         0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1];//down
+      
+		this.addEventListener(Event.ENTER_FRAME, this.walk);
+	}
+});
+
+var ArticunoEnemy = Class.create(Enemy, {
+	initialize: function(map, pathTile) {
+		Enemy.apply(this, ['assets/enemies/articunoSheet.png', 50, 30, map, pathTile]);
+
+      this.anim = [
+         9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 10, 10, 10, 10, 10, //right
+         6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 7, 7, 7, 7, 7,//left
+         3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 4, 4, 4, 4, 4, //up
+         0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1];//down
+      
+		this.addEventListener(Event.ENTER_FRAME, this.walk);
+	}
+});
+
+var PorygonEnemy = Class.create(Enemy, {
+	initialize: function(map, pathTile) {
+		Enemy.apply(this, ['assets/enemies/porygonSheet.png', 50, 30, map, pathTile]);
+
+      this.anim = [
+         9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 10, 10, 10, 10, 10, //right
+         6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 7, 7, 7, 7, 7,//left
+         3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 4, 4, 4, 4, 4, //up
+         0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1];//down
+      
+		this.addEventListener(Event.ENTER_FRAME, this.walk);
+	}
+});
+
+var SuicuneEnemy = Class.create(Enemy, {
+	initialize: function(map, pathTile) {
+		Enemy.apply(this, ['assets/enemies/suicuneSheet.png', 50, 30, map, pathTile]);
+
+      this.anim = [
+         6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, //right
+         4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, //left
+         2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, //up
+         0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1];//down
       
 		this.addEventListener(Event.ENTER_FRAME, this.walk);
 	}
