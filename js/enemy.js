af735c81e2c7c86c1a2d67af35523e45c359ba84
Dante Gagliardi <dantejgagliardi@gmail.com>
2013-12-09 14:58:36 -0800

    Scaled enemy stats

--- js/enemy.js
+++ js/enemy.js
@@ -151,7 +151,7 @@ var BastidonEnemy = Class.create(Enemy, {
 
 var AerodactylEnemy = Class.create(Enemy, {
 	initialize: function(map, pathTile) {
-		Enemy.apply(this, ['assets/enemies/aerodactylSheet.png', 30, 30, map, pathTile]);
+		Enemy.apply(this, ['assets/enemies/aerodactylSheet.png', 45, 30, map, pathTile]);
 
       this.anim = [
          9, 9, 9, 9, 9,10,10,10,10,10,11,11,11,11,11,10,10,10,10,10, //right
@@ -181,7 +181,7 @@ var AronEnemy = Class.create(Enemy, {
 
 var AggronEnemy = Class.create(Enemy, {
 	initialize: function(map, pathTile) {
-		Enemy.apply(this, ['assets/enemies/aggronSheet.png', 30, 40, map, pathTile]);
+		Enemy.apply(this, ['assets/enemies/aggronSheet.png', 60, 40, map, pathTile]);
 
       this.anim = [
          9, 9, 9, 9, 9,10,10,10,10,10,11,11,11,11,11,10,10,10,10,10, //right
@@ -195,7 +195,7 @@ var AggronEnemy = Class.create(Enemy, {
 
 var SkarmoryEnemy = Class.create(Enemy, {
 	initialize: function(map, pathTile) {
-		Enemy.apply(this, ['assets/enemies/skarmorySheet.png', 30, 40, map, pathTile]);
+		Enemy.apply(this, ['assets/enemies/skarmorySheet.png', 65, 40, map, pathTile]);
 
       this.anim = [
          9, 9, 9, 9, 9,10,10,10,10,10,11,11,11,11,11,10,10,10,10,10, //right
@@ -209,7 +209,7 @@ var SkarmoryEnemy = Class.create(Enemy, {
 
 var RyhornEnemy = Class.create(Enemy, {
 	initialize: function(map, pathTile) {
-		Enemy.apply(this, ['assets/enemies/ryhornSheet.png', 50, 30, map, pathTile]);
+		Enemy.apply(this, ['assets/enemies/ryhornSheet.png', 50, 45, map, pathTile]);
 
       this.anim = [
          6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, //right
@@ -225,7 +225,7 @@ var RyhornEnemy = Class.create(Enemy, {
 
 var GroudonEnemy = Class.create(Enemy, {
 	initialize: function(map, pathTile) {
-		Enemy.apply(this, ['assets/enemies/groudonSheet.png', 50, 30, map, pathTile]);
+		Enemy.apply(this, ['assets/enemies/groudonSheet.png', 100, 30, map, pathTile]);
 
       this.anim = [
          9, 9, 9, 9, 9,10,10,10,10,10,11,11,11,11,11,10,10,10,10,10, //right
@@ -239,7 +239,7 @@ var GroudonEnemy = Class.create(Enemy, {
 
 var ArticunoEnemy = Class.create(Enemy, {
 	initialize: function(map, pathTile) {
-		Enemy.apply(this, ['assets/enemies/articunoSheet.png', 50, 30, map, pathTile]);
+		Enemy.apply(this, ['assets/enemies/articunoSheet.png', 50, 60, map, pathTile]);
 
       this.anim = [
          9, 9, 9, 9, 9,10,10,10,10,10,11,11,11,11,11,10,10,10,10,10, //right
@@ -258,7 +258,7 @@ var ArticunoEnemy = Class.create(Enemy, {
 
 var PorygonEnemy = Class.create(Enemy, {
 	initialize: function(map, pathTile) {
-		Enemy.apply(this, ['assets/enemies/porygonSheet.png', 50, 30, map, pathTile]);
+		Enemy.apply(this, ['assets/enemies/porygonSheet.png', 35, 30, map, pathTile]);
 
       this.anim = [
          9, 9, 9, 9, 9,10,10,10,10,10,11,11,11,11,11,10,10,10,10,10, //right
@@ -272,7 +272,7 @@ var PorygonEnemy = Class.create(Enemy, {
 
 var SuicuneEnemy = Class.create(Enemy, {
 	initialize: function(map, pathTile) {
-		Enemy.apply(this, ['assets/enemies/suicuneSheet.png', 50, 30, map, pathTile]);
+		Enemy.apply(this, ['assets/enemies/suicuneSheet.png', 50, 80, map, pathTile]);
 
       this.anim = [
          6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, //right
